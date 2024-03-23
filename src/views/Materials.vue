a290eb6730cf0406bd24c420f1f111b6de696c87
mohhawamdeh <157696341+mohhawamdeh@users.noreply.github.com>
2024-03-23 19:55:25 +0300

    ff

--- src/views/Materials.vue
+++ src/views/Materials.vue
@@ -1,20 +1,24 @@
+
 <template>
 	<div>
 		<h1>Material List</h1>
-		<table>
+		<table class="material-table">
 			<thead>
 				<tr>
-					<th>ID</th>
+					<th>Trans ID</th>
 					<th>Name</th>
+					<th>Quantity</th>
+					<th>Logtime</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr v-for="material in materials.materials" :key="material.id">
-					<td>{{ material.trans_id }}</td>
+					<td>{{ truncateTransId(material.trans_id) }}</td>
 					<td>{{ material.name }}</td>
+					<td>{{ material.quantity }}</td>
+					<td>{{ material.logtime }}</td>
 				</tr>
 			</tbody>
-
 		</table>
 		<button @click="previousPage" :disabled="currentPage === 1">Previous Page</button>
 		<button @click="nextPage">Next Page</button>
@@ -24,6 +28,8 @@
 <script>
 import { ref } from 'vue';
 import { getAccount } from "@/web3Service.js" // Import the web3Service.js file
+
+
 
 export default {
 	setup() {
@@ -63,6 +69,57 @@ export default {
 			previousPage,
 			currentPage
 		};
+	},
+	data() {
+		return {
+			truncateTransId: function (transId) {
+				return transId.slice(0, 24) + '...';
+			}
+		};
 	}
 };
 </script>
+<style scoped>
+.material-table {
+	width: 100%;
+	border-collapse: separate;
+	/* Change to separate */
+	border-radius: 10px;
+	/* Add border radius */
+}
+
+.material-table th,
+.material-table td {
+	padding: 8px;
+	text-align: left;
+}
+
+.material-table th {
+	background-color: #CFCFCF;
+	color: black;
+	font-weight: bold;
+	text-align: center;
+	border: none;
+}
+
+.material-table tbody tr:nth-child(odd) {
+	background-color: #1e293b;
+}
+
+.material-table tbody tr:nth-child(even) {
+	background-color: #1e293b;
+}
+
+.material-table tbody tr td {
+	color: white;
+	text-align: center;
+}
+
+.material-table tbody tr {
+	border-bottom: 1px solid white;
+}
+
+button {
+	margin-top: 10px;
+}
+</style>
